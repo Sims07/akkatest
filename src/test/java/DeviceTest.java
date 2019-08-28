883ef488229f2afb0da6292447172732b5472e2b
Simon Chareyron <simonchareyron@MacBook-Pro-de-Simon.local>
2019-08-28 12:33:31 -0400

    ajout acteur device+tests

---
+++ src/test/java/DeviceTest.java
@@ -0,0 +1,37 @@
+import akka.actor.ActorRef;
+import akka.actor.ActorSystem;
+import akka.testkit.javadsl.TestKit;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import java.util.Optional;
+
+import static org.junit.Assert.assertEquals;
+
+public class DeviceTest {
+
+    static ActorSystem system;
+
+    @BeforeClass
+    public static void setup() {
+        system = ActorSystem.create();
+    }
+
+    @AfterClass
+    public static void teardown() {
+        TestKit.shutdownActorSystem(system);
+        system = null;
+    }
+
+    // #device-read-test
+    @Test
+    public void testReplyWithEmptyReadingIfNoTemperatureIsKnown() {
+        TestKit probe = new TestKit(system);
+        ActorRef deviceActor = system.actorOf(Device.props("group", "device"));
+        deviceActor.tell(new Device.ReadTemperature(42L), probe.getRef());
+        Device.RespondTemperature response = probe.expectMsgClass(Device.RespondTemperature.class);
+        assertEquals(42L, response.requestId);
+        assertEquals(Optional.empty(), response.value);
+    }
+}
